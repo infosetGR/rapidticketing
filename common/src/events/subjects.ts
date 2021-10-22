5961c6997185775f13e415d2ccae98acc2a54eee
infosetGR <info@infoset.co>
2021-10-23 02:23:40 +0300

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -3,5 +3,6 @@ export enum Subjects {
     TicketUpdated = 'ticket:updated',
     OrderCreated = 'order:created',
     OrderDeleted = 'order:deleted',
-    ExpirationOrder='expiration:order'
+    ExpirationOrder='expiration:order',
+    PaymentCreated='payment:created'
   }
